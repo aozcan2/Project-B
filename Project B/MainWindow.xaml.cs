de6e5ff93c77641af1b4a722d03f5e003143ecb6
aozcan2 <aozcan2@binghamton.edu>
2018-06-20 22:04:16 -0700

    miscellenious changes

--- Project B/MainWindow.xaml.cs
+++ Project B/MainWindow.xaml.cs
@@ -1,18 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Project_B
 {
@@ -26,6 +15,7 @@ namespace Project_B
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
         }
 
 
@@ -33,5 +23,6 @@ namespace Project_B
         {
             users.Add(item: new User() { UserName = username_box.ToString(), Password = password_box.Password.ToString(), LoginTime = DateTime.Now.ToString() });
         }
+        
     }
 }
